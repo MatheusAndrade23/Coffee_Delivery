06460a62b276375fc246c29fee945f57b7b76313
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-08-19 22:22:14 -0300

    HalfCoffeeCardComponent

--- src/components/HalfCoffeeCard/index.tsx
+++ src/components/HalfCoffeeCard/index.tsx
@@ -1,3 +1,33 @@
+import {
+  HalfCoffeeCardContainer,
+  CoffeeInfo,
+  CoffeeSale,
+  Remove,
+} from './styles';
+
+import { CoffeeAmount } from '../CoffeeAmount';
+
+import { FaRegTrashAlt } from 'react-icons/fa';
+
+import arabeCoffee from '../../assets/coffee-images/arabe.png';
+
 export const HalfCoffeeCard = () => {
-  return <div></div>;
+  return (
+    <HalfCoffeeCardContainer>
+      <img src={arabeCoffee} />
+      <div>
+        <CoffeeInfo>
+          <h6>Expresso Tradicional</h6>
+          <p>{`R$ ${700}`}</p>
+        </CoffeeInfo>
+        <CoffeeSale>
+          <CoffeeAmount amount={3} />
+          <Remove>
+            <FaRegTrashAlt />
+            <span>REMOVER</span>
+          </Remove>
+        </CoffeeSale>
+      </div>
+    </HalfCoffeeCardContainer>
+  );
 };
---
+++ src/components/HalfCoffeeCard/styles.ts
@@ -0,0 +1,76 @@
+import styled, { css } from 'styled-components';
+
+export const HalfCoffeeCardContainer = styled.div`
+  margin: 150px;
+  height: 8rem;
+  width: 36.8rem;
+  padding: 0 0.8rem;
+  background-color: ${({ theme }) => theme.colors['base-card']};
+  /* border-bottom: 1px solid ${({ theme }) => theme.colors['base-button']}; */
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  /* padding-bottom: 3rem; */
+
+  & img {
+    width: 6.4rem;
+  }
+
+  & > div {
+    height: 6.4rem;
+    width: 27.5rem;
+    margin-left: 2rem;
+  }
+`;
+
+export const CoffeeInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  height: 3.2rem;
+
+  h6 {
+    font-weight: normal;
+    font-size: ${({ theme }) => theme.fonts.sizes.small4};
+    font-family: ${({ theme }) => theme.fonts.family.primary};
+    color: ${({ theme }) => theme.colors['base-subtitle']};
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors['base-text']};
+    font-size: ${({ theme }) => theme.fonts.sizes.small4};
+    font-weight: bold;
+  }
+`;
+
+export const CoffeeSale = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 3.2rem;
+`;
+
+export const Remove = styled.button`
+  width: 9.1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-end;
+  background-color: ${({ theme }) => theme.colors['base-button']};
+  border-radius: 5px;
+  margin-left: 1rem;
+  border: none;
+
+  & span {
+    font-size: ${({ theme }) => theme.fonts.sizes.small2};
+    color: ${({ theme }) => theme.colors['base-text']};
+    margin-bottom: 0.8rem;
+    margin-left: 0.5rem;
+  }
+
+  & svg {
+    color: ${({ theme }) => theme.colors['5-color']};
+    font-size: 1.5rem;
+    margin-bottom: 0.8rem;
+  }
+`;
