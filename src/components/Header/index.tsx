4f803dfa972725d16dd055eeb0708b08ed2f2afe
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-08-21 00:15:19 -0300

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
+import { HeaderContainer } from './styles';
+
+import { GrLocation } from 'react-icons/gr';
+import { IoIosCart } from 'react-icons/io';
+
+import logo from '../../assets/logo.png';
+
 export const Header = () => {
-  return <div></div>;
+  return (
+    <HeaderContainer>
+      <img src={logo} />
+      <div>
+        <div>
+          <GrLocation />
+          <span>Minas Gerais, BR</span>
+        </div>
+        <a href="/">
+          <IoIosCart />
+        </a>
+      </div>
+    </HeaderContainer>
+  );
 };
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 10.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 3.8rem;
+    width: 19.3rem;
+
+    & div {
+      width: 14.3rem;
+      height: 100%;
+      border-radius: 5px;
+      padding: 0 0.8rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      background-color: ${({ theme }) => theme.colors['6-color']};
+      color: ${({ theme }) => theme.colors['4-color']};
+
+      & span {
+        font-size: ${({ theme }) => theme.fonts.sizes.small3};
+      }
+
+      & svg {
+        font-size: 2rem;
+      }
+
+      & path {
+        stroke: ${({ theme }) => theme.colors['4-color']};
+      }
+    }
+
+    & a {
+      height: 100%;
+      width: 3.8rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 5px;
+      font-size: 2.2rem;
+      background-color: ${({ theme }) => theme.colors['3-color']};
+      color: ${({ theme }) => theme.colors['1-color']};
+    }
+  }
+`;
