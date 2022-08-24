bf2e421d57608dd26572f62712aa145f1b682547
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-08-23 22:03:00 -0300

    Update index.ts

--- src/coffees/index.ts
+++ src/coffees/index.ts
@@ -21,6 +21,7 @@ const coffees = [
     description: 'O tradicional café feito com água quente e grãos moídos',
     srcImg: expresso,
     price: '5,00',
+    amount: 0,
   },
   {
     id: '2',
@@ -29,6 +30,7 @@ const coffees = [
     description: 'Expresso diluído, menos intenso que o tradicional',
     srcImg: americano,
     price: '4,00',
+    amount: 0,
   },
   {
     id: '3',
@@ -37,6 +39,7 @@ const coffees = [
     description: 'Café expresso tradicional com espuma cremosa',
     srcImg: expresso_cremoso,
     price: '6,00',
+    amount: 0,
   },
   {
     id: '4',
@@ -45,6 +48,7 @@ const coffees = [
     description: 'Bebida preparada com café expresso e cubos de gelo',
     srcImg: cafe_gelado,
     price: '7,00',
+    amount: 0,
   },
   {
     id: '5',
@@ -53,6 +57,7 @@ const coffees = [
     description: 'Meio a meio de expresso tradicional com leite vaporizado',
     srcImg: cafe_com_leite,
     price: '6,00',
+    amount: 0,
   },
   {
     id: '6',
@@ -62,6 +67,7 @@ const coffees = [
       'Uma dose de café expresso, com o dobro de leite e espuma cremosa',
     srcImg: latte,
     price: '8,00',
+    amount: 0,
   },
   {
     id: '7',
@@ -71,6 +77,7 @@ const coffees = [
       'Bebida com canela feita de doses iguais de café, leite e espuma',
     srcImg: capuccino,
     price: '10,00',
+    amount: 0,
   },
   {
     id: '8',
@@ -80,6 +87,7 @@ const coffees = [
       'Café expresso misturado com um pouco de leite quente e espuma',
     srcImg: macchiato,
     price: '9,00',
+    amount: 0,
   },
   {
     id: '9',
@@ -88,6 +96,7 @@ const coffees = [
     description: 'Café expresso com calda de chocolate, pouco leite e espuma',
     srcImg: mocaccino,
     price: '9,00',
+    amount: 0,
   },
   {
     id: '10',
@@ -96,6 +105,7 @@ const coffees = [
     description: 'Bebida feita com chocolate dissolvido no leite quente e café',
     srcImg: chocolate_quente,
     price: '10,00',
+    amount: 0,
   },
   {
     id: '11',
@@ -105,6 +115,7 @@ const coffees = [
       'Drink gelado de café expresso com rum, creme de leite e hortelã',
     srcImg: cubano,
     price: '10,00',
+    amount: 0,
   },
   {
     id: '12',
@@ -113,6 +124,7 @@ const coffees = [
     description: 'Bebida adocicada preparada com café e leite de coco',
     srcImg: havaiano,
     price: '9,00',
+    amount: 0,
   },
   {
     id: '13',
@@ -121,6 +133,7 @@ const coffees = [
     description: 'Bebida preparada com grãos de café árabe e especiarias',
     srcImg: arabe,
     price: '8,00',
+    amount: 0,
   },
   {
     id: '14',
@@ -129,6 +142,7 @@ const coffees = [
     description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
     srcImg: irlandes,
     price: '11,00',
+    amount: 0,
   },
   {
     id: '15',
@@ -138,6 +152,7 @@ const coffees = [
       'Uma dose de café expresso com  leite de soja e espuma cremosa',
     srcImg: latte,
     price: '11,00',
+    amount: 0,
   },
   {
     id: '16',
@@ -145,8 +160,8 @@ const coffees = [
     tags: ['Tradicional', 'Gelado'],
     description: 'Bebida preparada com café expresso americano e cubos de gelo',
     srcImg: americano,
-
     price: '4,00',
+    amount: 0,
   },
 ];
 
