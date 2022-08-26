c2f70a8b57cc31a336e1d4d5c39a677539a26fe5
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2022-08-25 22:07:51 -0300

    Update styled.d.ts

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,7 +1,7 @@
 import 'styled-components';
 import { lightTheme } from '../styles/themes/light';
 
-type ThemeType = typeof lightTheme;
+export type ThemeType = typeof lightTheme;
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
